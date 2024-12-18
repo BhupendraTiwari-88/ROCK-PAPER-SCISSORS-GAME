fb2c24730298b57ee0eea1a015f46ade70d115ae
Bhupendra Tiwari <bhupendratiwari886011@gmail.com>
2024-12-18 22:08:25 +0530

    Add files via upload

---
+++ ROCK-PAPER-SCISSORS GAME.py
@@ -0,0 +1,14 @@
+1 eco 
+2.print
+they both are same and use of print output screen the diffrnrcr is 
+1.eco has no return value while print return value 1 so you can be use in expression eco can take multiple paramter but print can take 1 argumenteco is faster than print we can eco as ("hello") print ca aslo use with also paranthess
+as we know srtirng by surrended by quotes but in php in diffent little bit variable can be inserted
+double quotes " VArible in the string are past and are replace there use values"
+single quotes' Varible are not past to we ned  to concatenate using the .(dot) operator'
+Example here plese paste here
+
+var-damp() =
+ in php usw eto disaply steructere infi. to the value it ype and the valuen is usinh oarticula	tr tbe debubbong purpoise it provide detailss info. include data type anmd den se info. let all typue of vatinrklre inclidinn g scalar , arary obke cyt amd null and values useful for debugginfg and the uns]=derstandijhg of data 
+ <? php
+ $number=10;
+ $number=10; 
